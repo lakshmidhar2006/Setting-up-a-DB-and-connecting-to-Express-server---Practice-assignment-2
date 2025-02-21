0b60cd59a7f04f4f8d7edd4e8e36fd09141fca33
unknown <santimallalakshmidhar@gmail.com>
2025-02-21 14:10:53 +0530

    xfgdf

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3010;
 
 app.use(express.json()); 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to database"))
   .catch(err => console.error("Error connecting to database:", err.message));
 
